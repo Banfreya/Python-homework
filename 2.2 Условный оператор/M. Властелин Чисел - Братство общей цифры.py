8383b91e98e2ed2399fc4847fb20b69420b38acb
Banfreya <banfreya@gmail.com>
2024-04-16 18:16:01 +0200

    2.2 M completed

---
+++ 2.2 Условный оператор/M. Властелин Чисел - Братство общей цифры.py
@@ -0,0 +1,7 @@
+elf_number = input()
+dwarf_number = input()
+human_number = input()
+if elf_number[0] == dwarf_number[0] == human_number[0]:
+    print(elf_number[0])
+else:
+    print(elf_number[1])
