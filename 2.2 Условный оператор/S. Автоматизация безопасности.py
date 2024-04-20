37ac12d161de8ef9d18790f9b2c3d8bc86584b52
Svetlana Popkova <s.popkova@nexters.com>
2024-04-20 22:09:10 +0200

    2.2 S Completed

--- 2.2 Условный оператор/S. Автоматизация безопасности.py
+++ 2.2 Условный оператор/S. Автоматизация безопасности.py
@@ -1,13 +1,14 @@
 x = float(input())
 y = float(input())
 island_circle_radius = 10
-danger_zone_radius = 5
-expedition_coordinates_projection = (x ** 2 + y ** 2) ** 0.5 
+expedition_coordinates_projection = (x**2 + y**2) ** 0.5 
+in_danger_zone_1 = x >= 0 and y >= 0 and expedition_coordinates_projection < 5
+in_danger_zone_2 = x < 0 and y <= 5 and y <= ((5 * x) + 35) / 3
+in_danger_zone_3_4 = y <= 0 and y < (0.25 * (x ** 2)) + (0.5 * x) + 8.75
 if expedition_coordinates_projection > island_circle_radius:
-        print("Вы вышли в море и рискуете быть съеденным акулой!")
-elif x >= 0 and y >= 0 and expedition_coordinates_projection < danger_zone_radius:
-        print("Опасность! Покиньте зону как можно скорее!")
-        
+    print("Вы вышли в море и рискуете быть съеденным акулой!")
+elif in_danger_zone_1 or in_danger_zone_2 or in_danger_zone_3_4:
+    print("Опасность! Покиньте зону как можно скорее!")
 else: 
-        print("Зона безопасна. Продолжайте работу.")
+    print("Зона безопасна. Продолжайте работу.")
 
