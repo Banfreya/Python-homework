21558da6c4ee4aa6842ba35fe7b71e9f323a6fec
Svetlana Popkova <s.popkova@nexters.com>
2024-04-29 18:51:42 +0200

    3.1 S, T completed

--- 3. Коллекции и работа с памятью/3.1. Строки, кортежи, списки/T. Польский калькулятор — 2.py
+++ 3. Коллекции и работа с памятью/3.1. Строки, кортежи, списки/T. Польский калькулятор — 2.py
@@ -42,20 +42,3 @@ for token in modified_N:
         operands.append(a)
         operands.append(c)
 print(operands[0])
-N = input()
-modified_N = N.split()
-operands = []
-for token in modified_N:
-    if token.isdigit():
-        operands.append(int(token))
-    elif token in ['+', '-', '*']:
-        operand_two = operands.pop()
-        operand_one = operands.pop()
-        if token == '+':
-            result = operand_one + operand_two
-        elif token == '-':
-            result = operand_one - operand_two
-        elif token == '*':
-            result = operand_one * operand_two
-        operands.append(result)
-print(operands[0])
