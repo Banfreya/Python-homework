2dfec94862d956dc85b835a3a3db40c9096eb698
Svetlana Popkova <s.popkova@nexters.com>
2024-06-06 21:04:22 +0200

    5.3 E Completed

---
+++ 5. Объектно-ориентированное программирование/5.3. Модель исключений Python. Try, except, else, finally. Модули/E. Слияние с проверкой.py
@@ -0,0 +1,32 @@
+# Когда-то вы уже писали функцию merge, которая производит слияние двух отсортированных кортежей.
+#
+# Давай-те её немного переработаем.
+#
+# Введём систему проверок:
+#
+# если один из параметров не является итерируемым объектом, то вызовите исключение StopIteration;
+# если значения входных параметров содержат «неоднородные» данные, то вызовите исключение TypeError;
+# если один из параметров не отсортирован, то вызовите исключение ValueError.
+# Проверки следует проводить в указанном порядке.
+#
+# Если параметры прошли все проверки, верните итерируемый объект, являющийся слиянием двух переданных.
+#
+# Примечание
+# В решении не должно быть вызовов требуемых функций.
+
+def merge(first_tuple, second_tuple):
+    if not hasattr(first_tuple, '__iter__') or not hasattr(second_tuple, '__iter__'):
+        raise StopIteration
+
+    first_tuple = tuple(first_tuple)
+    second_tuple = tuple(second_tuple)
+
+    if not first_tuple or not all([isinstance(x, type(first_tuple[0])) for x in first_tuple + second_tuple]):
+        raise TypeError
+
+    if first_tuple != tuple(sorted(first_tuple)) or second_tuple != tuple(sorted(second_tuple)):
+        raise ValueError
+
+    merged_list = sorted(first_tuple + second_tuple)
+    return tuple(merged_list)
+
